ee2158531b9baf91ea186927c4758a9be55d40d8
Dulin Hleb <dulingleb@gmail.com>
2021-01-21 14:32:03 +0300

    Users store, update, delete

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,28 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function validate(
+        Request $request,
+        array $rules,
+        array $messages = [],
+        array $customAttributes = [])
+    {
+        $validator = $this->getValidationFactory()
+            ->make(
+                $request->all(),
+                $rules, $messages,
+                $customAttributes
+            );
+        if ($validator->fails()) {
+            $errors = (new \Illuminate\Validation\ValidationException($validator))->errors();
+            throw new \Illuminate\Http\Exceptions\HttpResponseException(response()->json(
+                [
+                    'status' => false,
+                    'message' => "Some fields are missing!",
+                    'error_code' => 1,
+                    'errors' => $errors
+                ], \Illuminate\Http\JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+        }
+    }
 }
